35d0c4a14e4517ded62bcef5e8a42f62bb6bd25c
jiyunahito <jiyunahito11702@gmail.com>
2026-08-18 15:20:38 +0800

    新增key

---
+++ Core/Inc/key.h
@@ -0,0 +1,20 @@
+#ifndef KEY_H
+#define KEY_H
+
+#include "main.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef enum{
+    KEY_EVENT_NONE = 0,
+    KEY_EVENT_PRESS,
+    KEY_EVENT_RELEASE,
+    KET_EVENT_LONG_PRESS    
+} ekeyKeyEvent;
+
+void vkeyKey_Init(void);
+void vkeyKey_Tick(void);
+ekeyKeyEvent ekeyKey_GetEvent(void);
+bool bkey_Key_IsPressed(void);
+
+#endif
---
+++ Core/Src/key.c
@@ -0,0 +1,21 @@
+#include "key.h"
+
+static bool prvbkeyKey_Read(void){
+    
+}
+
+void vkeyKey_Init(void){
+
+}
+
+void vkeyKey_Tick(void){
+
+}
+
+ekeyKeyEvent ekeyKey_GetEvent(void){
+
+}
+
+bool bkey_Key_IsPressed(void){
+
+}
