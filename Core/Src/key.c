39832abeeae1f44a19be3d33314de49f08ca26fb
jiyunahito <jiyunahito11702@gmail.com>
2026-08-21 16:59:41 +0800

    新增

--- Core/Src/key.c
+++ Core/Src/key.c
@@ -1,9 +1,15 @@
 #include "key.h"
 
+#define KEY_ACTIVE_LEVEL 0 // active-low
+#define KEY_DEBOUNCE_MS 20U // 防彈跳延遲
+#define KEY_LONG_PRESS_MS 1000U // 長按所需時間
 
+typedef enum{
+    KEYSTATE_IDLE = 0,
+} KeyState;
 
 static bool prvbkeyKey_Read(void){
-    return  LL_GPIO_IsInputPinSet(KEY2_GPIO_Port, KEY2_Pin) == 0;
+    return  LL_GPIO_IsInputPinSet(KEY2_GPIO_Port, KEY2_Pin) == KEY_ACTIVE_LEVEL;
 }
 
 void vkeyKey_Init(void){
